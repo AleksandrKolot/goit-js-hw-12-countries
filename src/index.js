6189651cfc8c71c1c540581a11b27fbba906b7cd
Aleksandr Kolot <aleksandrkolot7@gmail.com>
2020-11-05 18:17:54 +0200

    search done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,66 @@
 import './styles.css';
+import fetchCountries from './fetchCountries.js';
+import countryListItemsTemplate from './tamplate/countryListItem.hbs';
+import countriesListTemplate from './tamplate/countriesList.hbs';
+import {info, error} from '@pnotify/core';
+import "@pnotify/core/dist/PNotify.css";
+import "@pnotify/core/dist/BrightTheme.css";
+import debounce from 'lodash.debounce';
+// 
+const refs = {
+  searchForm: document.querySelector('#search-form'),
+  countryList: document.querySelector('#country-list'),
+  searchInput: document.querySelector('.search__input'),
+};
+
+refs.searchForm.addEventListener('submit', event => {
+  event.preventDefault();
+});
+
+refs.searchForm.addEventListener(
+  'input',
+  debounce(e => {
+    searchFormInputHandler(e);
+  }, 500),
+);
+
+function searchFormInputHandler(e) {
+  const searchQuery = e.target.value;
+
+  clearListItems();
+
+  fetchCountries(searchQuery).then(data => {
+    const markup = buildListItemMarkup(data);
+    const renderCountriesList = buildCountriesList(data);
+    if (!data) {
+      return;
+    } else if (data.length > 10) {
+      error({
+        text: 'Введите полное название',
+      });
+    } else if (data.length >= 2 && data.length <= 10) {
+      insertListItem(renderCountriesList);
+    } else if (data.length === 1) {
+      insertListItem(markup);
+    } else {
+      info({ text: 'Не верное название' });
+    }
+  });
+  
+}
+
+function insertListItem(items) {
+  refs.countryList.insertAdjacentHTML('beforeend', items);
+}
+
+function buildCountriesList(items) {
+  return countriesListTemplate(items);
+}
+
+function buildListItemMarkup(items) {
+  return countryListItemsTemplate(items);
+}
+
+function clearListItems() {
+  refs.countryList.innerHTML = '';
+}
